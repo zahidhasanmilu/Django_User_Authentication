66da605cd43e9636641b2c50e9e6d03fcfa0d05d
zahidhasanmilu <minions.milu889@gmail.com>
2025-01-24 02:00:05 +0600

    login form and templates

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home,user_signup
+from .views import Home,user_signup,user_login
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('signup/', user_signup, name='signup'),
+    path('login/', user_login, name='login'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,19 +1,48 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .utils import logout_required
+
+# message
+from django.contrib import messages
 
 
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
-    
-    
+
+
+@logout_required
 def user_signup(request):
+    # if request.user.is_authenticated: #####if not use custom @logout_required decorator
+    #     return redirect('home')
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)   
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html',context={'form':form})
+    return render(request, 'signup.html', context={'form': form})
+
+
+def user_login(request):
+    # @logout_required  #####if not use request.user.is_authenticated:
+    if request.user.is_authenticated:
+        return redirect('home')
+    form = AuthenticationForm()
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+            print(form.errors)
+            messages.warning(request, 'Invalid username or password')
+            return redirect('login')
+    return render(request, 'login.html', context={'form': form})
